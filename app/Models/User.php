c069721630a8ec1919577557a935f72706333253
WizardIsHere <mshushant235@gmail.com>
2022-12-23 15:09:48 +0530

    second Commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,9 +20,9 @@ class User extends Authenticatable
     protected $fillable = [
         'fname',
         'mname',
-        'lname',
         'registration',
         'department',
+        'lname',
         'semester',
         'email',
         'address',
