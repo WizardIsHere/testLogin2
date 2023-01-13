1c82c8e0479a1f8f2c1957d516d9b344a8973921
SnehaElizabethRoy <120782422+SnehaElizabethRoy@users.noreply.github.com>
2023-01-13 13:20:15 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- WelcomeBlade -->
+        <!--  This is Welcome blade -->
         <title>Laravel</title>
 
         <!-- Fonts -->
